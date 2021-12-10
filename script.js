482cdac04837ecdf0624115860b83c1677ebb0d1
saint-lag <maiasena.gabriel@gmail.com>
2021-12-10 14:54:25 -0300

    Function: createsPixelBoard Implemented

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 // Pixel Board
-const pixelBoard = document.querySelector('#pixel-board');
-const pixelDiv = document.createElement('div');
-pixelDiv.style.width = '40px';
-pixelDiv.style.height = '40px';
-pixelDiv.style.border = '1px solid black';
-pixelDiv.style.display = 'inline-block';
-pixelDiv.className = 'pixel';
-pixelDiv.style.background = 'white';
+// Receives a number as input
+function createsPixelBoard(size) {
+  const intSize = parseInt(size);
+  const pixelBoard = document.querySelector('#pixel-board');
+  const pixelDiv = document.createElement('div');
+  pixelDiv.style.width = '40px';
+  pixelDiv.style.height = '40px';
+  pixelDiv.style.border = '1px solid black';
+  pixelDiv.style.display = 'inline-block';
+  pixelDiv.className = 'pixel';
+  pixelDiv.style.background = 'white';
+  pixelDiv.style.lineHeight = '0px';
 
-for (let i = 0; i < 25; i += 1) {
-  pixelBoard.appendChild(pixelDiv.cloneNode(true));
+  if (intSize < 50 && intSize > 5) {
+    for (let i = 0; i < intSize * intSize; i += 1) {
+      pixelBoard.appendChild(pixelDiv.cloneNode(true));
+    }
+  } else {
+    for (let i = 0; i < 25; i += 1) {
+      pixelBoard.appendChild(pixelDiv.cloneNode(true));
+    }
+  }
 }
+
+createsPixelBoard();
 
 // Random Color
 const randomColor = document.querySelector('#random-color');
@@ -65,6 +78,7 @@ const pixels = document.querySelectorAll('.pixel');
 
 for (let pixel of pixels) {
   pixel.addEventListener('click', function () {
+    pixel.style.transition = '0.5s';
     pixel.style.background = selectedColor;
   });
 }
@@ -89,6 +103,8 @@ clearBoardButton.addEventListener('click', function () {
   }
 });
 
+// CMYK Button
+
 const cyanMagentaYellowButton = document.querySelector('#cyan-magenta-yellow');
 
 cyanMagentaYellowButton.addEventListener('click', function () {
@@ -107,3 +123,28 @@ cyanMagentaYellowButton.addEventListener('mouseout', function (event) {
   event.target.style.transition = '0.5s';
   event.target.style.transform = 'scale(1.0)';
 });
+
+// Size Buttons
+
+const sizeButtons = document.querySelectorAll('.size-button');
+
+for (let button of sizeButtons) {
+  button.addEventListener('mouseover', function (event) {
+    event.target.style.transition = '0.5s';
+    event.target.style.transform = 'scale(1.05)';
+  });
+  button.addEventListener('mouseout', function (event) {
+    event.target.style.transition = '0.5s';
+    event.target.style.transform = 'scale(1.0)';
+  });
+  button.addEventListener('click', function (event) {
+    const pixelBoard = document.querySelector('#pixel-board');
+
+    while (pixelBoard.firstChild) {
+      pixelBoard.removeChild(pixelBoard.firstChild);
+    }
+
+    let size = parseInt(event.target.id[1]);
+    createsPixelBoard(size);
+  });
+}
