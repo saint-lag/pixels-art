0acc0e98db3cca283fc228d25af0edc6c5622ac3
saint-lag <maiasena.gabriel@gmail.com>
2021-12-09 23:37:35 -0300

    Bônus

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ pixelDiv.style.width = '40px';
 pixelDiv.style.height = '40px';
 pixelDiv.style.border = '1px solid black';
 pixelDiv.style.display = 'inline-block';
-pixelDiv.style.margin = '0.1em';
 pixelDiv.className = 'pixel';
+pixelDiv.style.background = 'white';
 
 for (let i = 0; i < 25; i += 1) {
   pixelBoard.appendChild(pixelDiv.cloneNode(true));
@@ -29,10 +29,22 @@ const availableColors = [
   'purple',
 ];
 
-randomColor.addEventListener('click', function () {
-  const color = parseInt(Math.random() * (availableColors.length + 1 - 0) + 0);
-  randomColor.style.background = availableColors[color];
-});
+// Not First Visit
+
+const colorsButtons = document.querySelectorAll('.color');
+if (localStorage.getItem('sessionInitiated') === 'true') {
+  console.log('hi');
+  for (let button of colorsButtons) {
+    console.log('hi');
+    const color = parseInt(
+      Math.random() * (availableColors.length + 1 - 0) + 0
+    );
+    button.style.background = availableColors[color];
+    document.getElementById('black').style.background = 'black';
+  }
+} else {
+  localStorage.setItem('sessionInitiated', JSON.stringify(true));
+}
 
 // Selected Color
 const colorsButton = document.querySelectorAll('.color');
@@ -60,8 +72,38 @@ for (let pixel of pixels) {
 // Clear Board
 const clearBoardButton = document.querySelector('#clear-board');
 
+clearBoardButton.addEventListener('mouseover', function (event) {
+  event.target.style.transition = '0.5s';
+  event.target.style.transform = 'scale(1.05)';
+});
+
+clearBoardButton.addEventListener('mouseout', function (event) {
+  event.target.style.transition = '0.5s';
+  event.target.style.transform = 'scale(1.0)';
+});
+
 clearBoardButton.addEventListener('click', function () {
   for (let pixel of pixels) {
     pixel.style.background = 'white';
+    pixel.style.transition = '1.2s';
   }
+});
+
+const cyanMagentaYellowButton = document.querySelector('#cyan-magenta-yellow');
+
+cyanMagentaYellowButton.addEventListener('click', function () {
+  document.querySelector('#cyan').style.background = 'cyan';
+  document.querySelector('#magenta').style.background = 'magenta';
+  document.querySelector('#yellow').style.background = 'yellow';
+  document.querySelector('#random-color').style.background = 'whitesmoke';
+});
+
+cyanMagentaYellowButton.addEventListener('mouseover', function (event) {
+  event.target.style.transition = '0.5s';
+  event.target.style.transform = 'scale(1.05)';
+});
+
+cyanMagentaYellowButton.addEventListener('mouseout', function (event) {
+  event.target.style.transition = '0.5s';
+  event.target.style.transform = 'scale(1.0)';
 });
